b9055cedc86a8aaf5a8586f47ec2bfc871ae6649
yashika-aggarwal <yashika.070898@gmail.com>
2022-05-18 13:14:08 +0530

    added a solution to delete node in a linked list  problem

---
+++ Easy/DeleteNodeInaLinkedList/Solution1.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    let tail = node.next;
+    node.val = tail.val;
+    node.next = tail.next;
+};
